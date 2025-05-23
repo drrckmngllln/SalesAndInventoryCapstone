71d9bfb588957ce441d2fafea9070134237d8a5f
Derrick Manglallan <manglallan.derrick@gmail.com>
2025-05-23 16:41:47 +0800

    feat: added title name to the login form

--- SalesAndInventoryCapstone/Forms/LoginForm.Designer.vb
+++ SalesAndInventoryCapstone/Forms/LoginForm.Designer.vb
@@ -29,15 +29,20 @@ Partial Class LoginForm
         btnExit = New Krypton.Toolkit.KryptonButton()
         PictureBox1 = New PictureBox()
         Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
         CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
         ' tUsername
         ' 
         tUsername.CueHint.CueHintText = "Username"
-        tUsername.Location = New Point(152, 216)
+        tUsername.Location = New Point(11, 308)
         tUsername.Name = "tUsername"
-        tUsername.Size = New Size(355, 31)
+        tUsername.Size = New Size(374, 31)
         tUsername.StateActive.Border.Rounding = 10F
         tUsername.StateActive.Border.Width = 2
         tUsername.TabIndex = 0
@@ -45,10 +50,10 @@ Partial Class LoginForm
         ' tPassword
         ' 
         tPassword.CueHint.CueHintText = "Password"
-        tPassword.Location = New Point(152, 253)
+        tPassword.Location = New Point(11, 345)
         tPassword.Name = "tPassword"
         tPassword.PasswordChar = "●"c
-        tPassword.Size = New Size(355, 31)
+        tPassword.Size = New Size(374, 31)
         tPassword.StateActive.Border.Rounding = 10F
         tPassword.StateActive.Border.Width = 2
         tPassword.TabIndex = 1
@@ -56,7 +61,7 @@ Partial Class LoginForm
         ' 
         ' btnLogin
         ' 
-        btnLogin.Location = New Point(190, 290)
+        btnLogin.Location = New Point(61, 382)
         btnLogin.Name = "btnLogin"
         btnLogin.Size = New Size(126, 33)
         btnLogin.StateCommon.Back.Color1 = Color.FromArgb(CByte(0), CByte(0), CByte(50))
@@ -72,7 +77,7 @@ Partial Class LoginForm
         ' 
         ' btnExit
         ' 
-        btnExit.Location = New Point(322, 290)
+        btnExit.Location = New Point(193, 382)
         btnExit.Name = "btnExit"
         btnExit.Size = New Size(126, 33)
         btnExit.StateCommon.Back.Color1 = Color.FromArgb(CByte(0), CByte(0), CByte(50))
@@ -89,9 +94,9 @@ Partial Class LoginForm
         ' PictureBox1
         ' 
         PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
-        PictureBox1.Location = New Point(255, -3)
+        PictureBox1.Location = New Point(134, 12)
         PictureBox1.Name = "PictureBox1"
-        PictureBox1.Size = New Size(153, 142)
+        PictureBox1.Size = New Size(112, 105)
         PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
         PictureBox1.TabIndex = 4
         PictureBox1.TabStop = False
@@ -101,19 +106,79 @@ Partial Class LoginForm
         Label1.AutoSize = True
         Label1.Font = New Font("Segoe UI Semibold", 20F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
         Label1.ForeColor = SystemColors.Control
-        Label1.Location = New Point(287, 155)
+        Label1.Location = New Point(146, 261)
         Label1.Name = "Label1"
         Label1.Size = New Size(85, 37)
         Label1.TabIndex = 5
         Label1.Text = "Login"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label2.ForeColor = SystemColors.Control
+        Label2.Location = New Point(92, 120)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(210, 21)
+        Label2.TabIndex = 6
+        Label2.Text = "Sales and Inventory System"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label3.ForeColor = SystemColors.Control
+        Label3.Location = New Point(180, 141)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(26, 21)
+        Label3.TabIndex = 7
+        Label3.Text = "of"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label4.ForeColor = SystemColors.Control
+        Label4.Location = New Point(68, 162)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(264, 21)
+        Label4.TabIndex = 8
+        Label4.Text = "Repear Parts and Farm Machineries"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label5.ForeColor = SystemColors.Control
+        Label5.Location = New Point(180, 183)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(24, 21)
+        Label5.TabIndex = 9
+        Label5.Text = "at"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI Semibold", 15F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label6.ForeColor = SystemColors.Control
+        Label6.Location = New Point(81, 204)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(241, 28)
+        Label6.TabIndex = 10
+        Label6.Text = "Agri Top Solana, Cagayan"
         ' 
         ' LoginForm
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         BackColor = Color.FromArgb(CByte(30), CByte(30), CByte(30))
-        ClientSize = New Size(650, 355)
+        ClientSize = New Size(399, 452)
         ControlBox = False
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
         Controls.Add(Label1)
         Controls.Add(PictureBox1)
         Controls.Add(btnExit)
@@ -146,4 +211,9 @@ Partial Class LoginForm
     Friend WithEvents btnExit As Krypton.Toolkit.KryptonButton
     Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
 End Class
